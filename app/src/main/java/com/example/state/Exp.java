6c305a8644b786ca7d9ce7845da3bb7560b0b583
DADJOD <kamu57@mail.ru>
2023-04-30 15:22:03 +0300

    commit_2

---
+++ app/src/main/java/com/example/state/Exp.java
@@ -0,0 +1,12 @@
+//package com.example.state;
+//
+//import android.view.View;
+//
+//public class Exp {
+//
+//    @Override
+//    public void onClick(View view) {
+//        count++;
+//        text.setText("" + count);
+//    }
+//}
--- app/src/main/java/com/example/state/MainActivity.kt
+++ app/src/main/java/com/example/state/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.state
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var count = 0
+    lateinit var buttonFirst: Button
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonFirst = findViewById(R.id.button1)
+        textView = findViewById(R.id.textView)
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(KEY_COUNT)
+        }
+
+        textView.text = "$count"
+        buttonFirst.setOnClickListener(this)
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_COUNT, count)
+    }
+
+    override fun onClick(v: View?) {
+        count++
+        textView.text = "$count"
+    }
+
+    companion object {
+        const val KEY_COUNT = "COUNT"
+    }
+
+//    override fun onClick(view: View?) {
+//        count++
+//        textView.text = "" + count
+//    }
 }
